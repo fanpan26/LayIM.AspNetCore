88b7d64572259ce7833f071e4cb47258cd3c907e
fanpan26 <fanpan26@126.com>
2018-05-29 09:29:42 +0800

    定义调用IMServer接口

---
+++ src/LayIM.AspNetCore/LayIM.AspNetCore.Middleware/IM/ILayIMServer.cs
@@ -0,0 +1,16 @@
+﻿using LayIM.AspNetCore.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LayIM.AspNetCore.Core.IM
+{
+
+    public interface ILayIMServer
+    {
+        TokenResult GetToken(string userId);
+        LayIMBaseResult SendPrivateMessage(string targetId, object message);
+        LayIMBaseResult SendGroupMessage(string targetId, object message);
+        LayIMBaseResult SendSystemMessage(string targetId, object message);
+    }
+}
